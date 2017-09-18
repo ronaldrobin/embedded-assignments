80941b3f10680be74315559e405352501b7bf13a
ronaldrobin <31402347+ronaldrobin@users.noreply.github.com>
2017-09-18 19:51:46 +0530

    Update strcopy.c

--- 14-sept/strcopy.c
+++ 14-sept/strcopy.c
@@ -3,18 +3,16 @@
 main()
 {
 	char array[100],array1[100],temp;
-	int i,;
+	int i;
 	printf("enter the string =");
 	i=0;
-	temp=0;
 	scanf("%s",&array);
 	for (i = 0; array[i] != '\0'; i++)
     {
         array1[i]=array[i];
         
     }
-    printf("\n the content of the array is %s",array);
-    
-    printf("\n the content of the array1 is %s",array1);
+     printf("\n the content of the array is %s",array);
+     printf("\n the content of the array1 is %s",array1);
     
 }
