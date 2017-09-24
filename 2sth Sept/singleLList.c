43595e075dd0b7f287c96f3934f99158865425d3
ronaldrobin <31402347+ronaldrobin@users.noreply.github.com>
2017-09-24 17:45:28 +0530

    Add files via upload

---
+++ 2sth Sept/singleLList.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+typedef struct node 
+    {
+       int data ;
+       struct node *next ;
+    } NODE ;
+    
+    /* Declare head of the list as a Global Variable */
+    NODE  *head ;
+    NODE  *scanPtr ;
+  
+  DisplayMenu()
+  {
+  	int i=0;
+  	
+	printf("\n 1. Create list");
+  	printf("\n 2. Add Node");
+  	printf("\n 3. Delete Node");
+  	printf("\n 4. Display list");
+  	printf("\n 5. Delete list");
+  	printf("\n 6. Search List");
+	 scanf(" %d ",&i);
+	 return i;
+	  }
+CreateList()
+{
+	  }	  
+	  
+AddNode()
+{
+	  }	  
+	  
+DeleteNode()
+{
+	  }	  
+DisplayList()
+{
+	int j=0;
+	if(j==0)
+	{
+		printf("\n The List is empty. ");
+	}
+	  }	  
+	  
+DeleteList()
+{
+	  }	  
+	      
+ SearchList()
+ {
+ 	
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+    int main()
+    {
+        int flag ;
+        int choice ;
+        flag = 1 ;
+        
+        while (flag ==1)
+        {
+             choice = DisplayMenu() ;
+             switch(choice)
+             {  case 1 :
+                { 
+                     CreateList() ;
+                     break ;
+                }
+                case 2:
+                {
+                     AddNode();
+                     break ;
+                }
+                case 3:
+                {
+                     DeleteNode();
+                     break ;
+                }
+                case 4:
+                {
+                    DisplayList() ;
+                    break ;
+                }
+                case 5:
+                {
+                     DeleteList() ; /* travese through the list and remove each element by element */
+                     break ;
+                }
+                case 6:
+                {
+                    SearchList();
+                    break ;
+                }
+                case 7:
+                {
+                    flag = 0 ;
+                    break ;
+                }
+                default:
+                {
+                   break ;
+                }
+             }
+        }
+    }
